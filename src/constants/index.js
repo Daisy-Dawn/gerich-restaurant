600f2b4da4165cd4e29cfe26046e402ceb8c3938
Daisy-Dawn <agboella20@gmail.com>
2023-12-22 09:17:28 +0100

    updated import

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
 import images from './images';
-import dining1 from '../assets/dinin1.mp4';
+import dining1 from '../assets/dining1.mp4';
 import data from './data';
 
 export { images, dining1, data };
